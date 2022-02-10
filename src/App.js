28f2b5c97253374fec72eac3f71b6d871fb5f715
BerchenkoDmytro <bera1795@rambler.ru>
2022-02-10 18:29:09 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className={'item'}>
+
+    <div>
+    <h1>Brand: Apple</h1>
+    <h2>Model: Pro</h2>
+    <h2>Cost: 100</h2>
+    <img src={'https://s1.stc.all.kpcdn.net/putevoditel/projectid_346574/images/tild3930-3137-4530-b661-346465643363__mac-pro-4.jpg'} alt="Apple"/>
+  </div>
+    <div>
+    <h1>Brand: HP</h1>
+    <h2>Model: 15s</h2>
+    <h2>Cost: 300</h2>
+    <img src={'https://www.hp.com/gb-en/shop/Html/Merch/Images/c06751173_390x286.jpg'} alt="HP_15s"/>
+  </div>
+
+  </div>
   );
 }
 

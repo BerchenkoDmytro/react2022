4989f089c48cc67f4ac7ee43d1ff82e9a5bdd4ee
BerchenkoDmytro <bera1795@rambler.ru>
2022-02-16 15:16:02 +0200

    react

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
   <div className={'item'}>
 
 
-    <Notebook brand={'Apple'} model={'PRO'} cost={'100'} img={'https://appleinsider.ru/wp-content/uploads/2021/10/new_mbp_14_16_2_inches-750x484.jpg'}/>
-    <Notebook brand={'HP'} model={'15s'} cost={'300'} img={'https://www.hp.com/gb-en/shop/Html/Merch/Images/c06751173_390x286.jpg'}/>
-    <Notebook brand={'ASUS'} model={'17XS'} cost={'500'} img={'https://itc.ua/wp-content/uploads/2021/10/asus-vivobook-15-oled-3.jpg'}/>
 
   </div>
   );
